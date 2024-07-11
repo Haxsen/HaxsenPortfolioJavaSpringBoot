d7a10346361b3b9820e27cd600ddce8bbd66f98a
DESKTOP-UUAFPH9\TheHxn <hassaan.ali.lel@gmail.com>
2024-07-11 17:34:09 +0500

    Added springboot project

---
+++ src/main/java/com/haxsen/HaxsenPortfolioJavaSpringBoot/HaxsenPortfolioJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.haxsen.HaxsenPortfolioJavaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaxsenPortfolioJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaxsenPortfolioJavaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haxsen/HaxsenPortfolioJavaSpringBoot/HaxsenPortfolioJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haxsen.HaxsenPortfolioJavaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaxsenPortfolioJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
